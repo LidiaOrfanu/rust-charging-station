dd8b04202528ab76f8979b20b65bc261f53615a6
Luc-Siat <lucsiat@gmail.com>
2023-07-20 11:16:41 +0200

    add handleHello function

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/hello", get(|| async { "Hello, World!" }));
+    // let app = Router::new().route("/hello", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/hello", get(handleHello));
 
-    // run it with hyper on localhost:3000
+
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
+
+async fn handleHello() -> &'static str {
+    return "Hello, World!";
+}
+7
